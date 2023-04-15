99f8014f423ecb1f49fa15ee3dd51033a3920350
kwguo <shana0440@gmail.com>
2023-04-15 11:39:36 +0800

    feat: add translate option to context menu

--- background.ts
+++ background.ts
@@ -1,3 +1,5 @@
+import { Storage } from "@plasmohq/storage"
+
 import { translate } from "~app/translator"
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -8,4 +10,21 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   )
   // keeps the channel open for sendResponse
   return true
+})
+
+const InlineTranslateId = "inline-translate"
+
+chrome.contextMenus.create({
+  id: InlineTranslateId,
+  title: "Inline Translate",
+  contexts: ["all"]
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === InlineTranslateId) {
+    const storage = new Storage()
+    storage.get("language").then((language) => {
+      chrome.tabs.sendMessage(tab.id, { language })
+    })
+  }
 })
