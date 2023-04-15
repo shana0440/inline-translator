26f3c775c508dbc7a4ff7ab5cfea501dac244351
kwguo <shana0440@gmail.com>
2023-04-15 10:34:55 +0800

    fix: layout broken in stack overflow

--- content.ts
+++ content.ts
@@ -10,15 +10,11 @@ const TRANSLATED_KEY = "translated"
 chrome.runtime.onMessage.addListener(() => {
   const heads = document.querySelectorAll<HTMLHeadElement>(
     Array.apply(null, Array(6))
-      .map((_, index) => `h${index + 1}:not([data-${TRANSLATED_KEY}])`)
+      .map((_, index) => `h${index + 1}`)
       .join(",")
   )
-  const paragraphs = document.querySelectorAll<HTMLParagraphElement>(
-    `p:not([data-${TRANSLATED_KEY}])`
-  )
-  const items = document.querySelectorAll<HTMLLIElement>(
-    `li:not([data-${TRANSLATED_KEY}])`
-  )
+  const paragraphs = document.querySelectorAll<HTMLParagraphElement>(`p`)
+  const items = document.querySelectorAll<HTMLLIElement>(`li`)
   const translateElements: TagForTranslateElements[] = [
     {
       elements: heads,
@@ -35,7 +31,10 @@ chrome.runtime.onMessage.addListener(() => {
   ]
 
   translateElements.forEach(({ elements, tag }) => {
-    elements.forEach((it) => {
+    getLeafNodes(elements).forEach((it) => {
+      if (it.dataset[TRANSLATED_KEY]) {
+        return
+      }
       chrome.runtime.sendMessage({ text: it.innerText }, (resp) => {
         const injectedNode = injectAfter(it, tag, resp.text)
         injectedNode.dataset[TRANSLATED_KEY] = "true"
@@ -44,3 +43,20 @@ chrome.runtime.onMessage.addListener(() => {
     })
   })
 })
+
+function getLeafNodes(
+  nodes: NodeListOf<HTMLElement> | NodeListOf<ChildNode>,
+  result = []
+) {
+  for (var i = 0, length = nodes.length; i < length; i++) {
+    const isLeaf = [...nodes[i].childNodes].some(
+      (it) => it.nodeType === Node.TEXT_NODE && (it as Text).data.trim() !== ""
+    )
+    if (isLeaf) {
+      result.push(nodes[i])
+    } else {
+      result = getLeafNodes(nodes[i].childNodes, result)
+    }
+  }
+  return result
+}
